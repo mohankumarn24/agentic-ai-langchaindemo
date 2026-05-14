e3a60c6a5c7dd394d6295a6de9e942746de9829e
mohankumarn24 <mohankumar.n24@outlook.com>
2026-05-14 20:20:40 +0530

    refactor

--- s14_use-deepseek-models/1_deepseek_demo.py
+++ s14_use-deepseek-models/1_deepseek_demo.py
@@ -4,13 +4,33 @@ llm = ChatOllama(model="deepseek-r1:1.5b")
 
 question = input("Enter question: ")
 response = llm.invoke(question)
+
+## Optional: If using ChatOllama
+# response = llm.invoke([
+#     (
+#         "system", 
+#         "You are a factual assistant. Answer in one sentence. Do not invent details. If unsure, say I don't know."),
+#     (
+#         "human", question
+#     )
+# ])
+
 print(response.content)
 
-# DeepSeek reasoning models sometimes internally generate
-# <think>
-#   step-by-step reasoning
-# </think>
-
 ## Run
+# TODO: May stress CPU
 # cd D:\dev\github\agentic-ai-langchaindemo\s14_use-deepseek-models
 # python 1_deepseek_demo.py
+
+
+## Output:
+# DeepSeek reasoning models sometimes internally generate <think>.
+# But, DeepSeek may not always show it
+#
+#   <think>
+#       step-by-step reasoning
+#   </think>
+# 
+# Questions to ask:
+# Q: If a train travels 60 km in 1.5 hours, what is its speed?
+# Q: Where is Taj Mahal?
--- s14_use-deepseek-models/2_deepseek_rag_demo.py
+++ s14_use-deepseek-models/2_deepseek_rag_demo.py
@@ -1,6 +1,6 @@
 import os
+
 from langchain_community.chat_models import ChatOllama
-from langchain_openai import OpenAIEmbeddings,ChatOpenAI
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
@@ -19,24 +19,22 @@ chunks = text_splitter.split_documents(document)
 vector_store = Chroma.from_documents(chunks, embeddings)
 retriever = vector_store.as_retriever()
 
-prompt_template = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """
-            You are an assistant for answering questions.
-            Use the provided context to respond.If the answer 
-            isn't clear, acknowledge that you don't know. 
-            Limit your response to three concise sentences.
-            {context}
-            """
-        ),
-        (
-            "human", 
-            "{input}"
-        )
-    ]
-)
+prompt_template = ChatPromptTemplate.from_messages([
+    (
+        "system",
+        """
+        You are an assistant for answering questions.
+        Use the provided context to respond.If the answer 
+        isn't clear, acknowledge that you don't know. 
+        Limit your response to three concise sentences.
+        {context}
+        """
+    ),
+    (
+        "human", 
+        "{input}"
+    )
+])
 
 qa_chain = create_stuff_documents_chain(llm, prompt_template)
 rag_chain = create_retrieval_chain(retriever, qa_chain)
@@ -45,11 +43,12 @@ print("Chat with Deepseek Document")
 question=input("Your Question: ")
 
 if question:
-    response = rag_chain.invoke({"input":question})
+    response = rag_chain.invoke({"input": question})
     print(response['answer'])
 
 
 ## Run
+# TODO: May stress CPU
 # cd D:\dev\github\agentic-ai-langchaindemo\s14_use-deepseek-models
 # python 2_deepseek_rag_demo.py   
 #
